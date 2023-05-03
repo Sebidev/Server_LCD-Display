c70c8a7d7b06ba1e8d556c4255c3e5c7ef53d013
Sebidev <sebidev@hotmail.com>
2023-05-03 20:58:08 +0200

    initial commit

--- server-lcddisplay.py
+++ server-lcddisplay.py
@@ -3,10 +3,10 @@ import time
 
 lcddisplay = serial.Serial('COM3', baudrate=9600, bytesize=8, parity='N', stopbits=1)
 
-text1 = 'hello world!1234567890'
+text = 'hello world!1234567890'
 
 time.sleep(3)
 
 lcddisplay.write(bytes(text1, encoding="ascii")) 
 lcddisplay.close()
-print(text1)
+print(text)

e88f55396cdefa93d78ed89d8d108a3437c315e9
sebidev <sebidev@hotmail.com>
2023-05-09 16:24:03 +0200

    commit '0002

--- server-lcddisplay.py
+++ server-lcddisplay.py
@@ -1,17 +1,23 @@
 import netifaces as ni
+import argparse
 import serial
 import time
-import sys
 import io
 
-ip = ni.ifaddresses(sys.argv[2])[ni.AF_INET][0]['addr']
+parser = argparse.ArgumentParser()
+parser.add_argument("-sp", "--serialport", required=True)
+parser.add_argument("-n", "--nic", required=True)
+args = parser.parse_args()
+
+print("Serialport: ", args.serialport, " NIC:", args.nic)
+
+ip = ni.ifaddresses(args.nic)[ni.AF_INET][0]['addr']
 loadavg = open("/proc/loadavg").readline().split(" ")[:3]
+
 text = 'IP:', ip, loadavg
 
-print("Serialport: ", sys.argv[1], " NIC:", sys.argv[2])
-#lcddisplay = serial.Serial(sys.argv[1], baudrate=9600, bytesize=8, parity='N', stopbits=1)
-
-time.sleep(1.5)
+#lcddisplay = serial.Serial(serialport, baudrate=9600, bytesize=8, parity='N', stopbits=1)
+#time.sleep(1.5)
 #lcddisplay.write(bytes(text, encoding="ascii")) 
 #lcddisplay.close()
 print(text)

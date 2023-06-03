eb3352ca160d741499c23ab4cfe2e4e9c2ca7b47
Sebidev <sebidev@hotmail.com>
2023-06-03 15:42:14 +0200

    0003

--- server-lcddisplay.py
+++ server-lcddisplay.py
@@ -6,7 +6,7 @@ import io
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-sp", "--serialport", required=True)
-parser.add_argument("-n", "--nic", required=True)
+parser.add_argument("-n", "--nic", required=False, default="lo")
 args = parser.parse_args()
 
 print("Serialport: ", args.serialport, " NIC:", args.nic)
@@ -14,10 +14,11 @@ print("Serialport: ", args.serialport, " NIC:", args.nic)
 ip = ni.ifaddresses(args.nic)[ni.AF_INET][0]['addr']
 loadavg = open("/proc/loadavg").readline().split(" ")[:3]
 
-text = 'IP:', ip, loadavg
+text = '{}  {}'.format(ip, ' '.join(loadavg))
 
-#lcddisplay = serial.Serial(serialport, baudrate=9600, bytesize=8, parity='N', stopbits=1)
-#time.sleep(1.5)
-#lcddisplay.write(bytes(text, encoding="ascii")) 
-#lcddisplay.close()
-print(text)
+lcddisplay = serial.Serial(args.serialport, baudrate=9600, bytesize=8, parity='N', stopbits=1)
+time.sleep(3)
+lcddisplay.write(bytes(str(text), encoding="ascii"))
+time.sleep(3)
+lcddisplay.close()
+print("Output: " + text)

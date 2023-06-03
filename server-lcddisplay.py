88a6b051778442bc0ba3b47e04e5ce94a86bc256
Sebidev <sebidev@hotmail.com>
2023-06-03 17:52:08 +0200

    0004

--- server-lcddisplay.py
+++ server-lcddisplay.py
@@ -13,15 +13,15 @@ args = parser.parse_args()
 ip = ni.ifaddresses(args.nic)[ni.AF_INET][0]['addr']
 loadavg = open("/proc/loadavg").readline().split(" ")[:3]
 
-text = '{}  {}'.format(ip, ' '.join(loadavg))
+output = '{}  {}'.format(ip, ' '.join(loadavg))
 
 if args.message:
-    text = args.message
+    output = args.message
 
 lcddisplay = serial.Serial(args.serialport, baudrate=9600, bytesize=8, parity='N', stopbits=1)
 time.sleep(3)
-lcddisplay.write(bytes(str(text), encoding="ascii"))
+lcddisplay.write(bytes(str(output), encoding="ascii"))
 time.sleep(3)
 lcddisplay.close()
 
-print("Output:" + text)
+print("Output:" + output)
